15e5b188dd24b3aff7348e12ff8a27a7a813f3f6
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2026-01-10 17:47:57 -0300

    refactor(player): simplify aim handling and cursor locking logic

--- Assets/_MAIN/Scripts/Player/Components/PlayerAim.cs
+++ Assets/_MAIN/Scripts/Player/Components/PlayerAim.cs
@@ -8,67 +8,61 @@ namespace RAIL_SHOOTER.PLAYER
     public class PlayerAim : PlayerComponent
     {
         private InputReader _inputReader;
-        [SerializeField] private bool _hideCursor = true;
-        [SerializeField] private float _sensitivity = 2.0f;
 
+        [SerializeField] private bool _hideCursor = true;
         [SerializeField] private RectTransform _crosshair;
 
         public override void OnAwake()
         {
             _inputReader = new InputReader();
         }
+
         public override void OnStart()
         {
             if (_hideCursor)
             {
                 Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.lockState = CursorLockMode.Confined;
             }
         }
+
         public override void OnEnable()
         {
             _inputReader.OnEnable();
             InputReader.OnLookInput += HandleLookInput;
         }
+
         public override void OnDisable()
         {
             _inputReader.OnDisable();
             InputReader.OnLookInput -= HandleLookInput;
         }
+
         private void HandleLookInput(Vector2 lookInput)
         {
-            float mouseX = lookInput.x * _sensitivity;
-            float mouseY = lookInput.y * _sensitivity;
+            Vector2 mouseScreenPos = Input.mousePosition;
+            Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
+            Vector2 relativePosition = mouseScreenPos - screenCenter;
 
-            (Vector2 minBounds, Vector2 maxBounds) = GetScreenBounds();
-
-            Vector3 currentPosition = _crosshair.anchoredPosition;
-            currentPosition += new Vector3(mouseX, mouseY, 0);
-
-            currentPosition.x = Mathf.Clamp(currentPosition.x, minBounds.x, maxBounds.x);
-            currentPosition.y = Mathf.Clamp(currentPosition.y, minBounds.y, maxBounds.y);
-
-            _crosshair.anchoredPosition = currentPosition;
+            _crosshair.anchoredPosition = relativePosition;
         }
-        public Vector3 GetAimWorldPosition(float distance)
+
+        public bool TryGetAimPoint(out Vector3 aimPoint, float maxDistance)
         {
             Camera cam = Camera.main;
 
-            Vector2 screenCenter = new Vector2(Screen.width / 2f, Screen.height / 2f);
-            Vector2 crosshairScreenPos = screenCenter + _crosshair.anchoredPosition;
+            Vector2 mouseScreenPos = Input.mousePosition;
 
-            Ray ray = cam.ScreenPointToRay(crosshairScreenPos);
-            return ray.origin + ray.direction * distance;
-        }
-        private static (Vector2, Vector2) GetScreenBounds()
-        {
-            int screenWidth = Screen.width;
-            int screenHeight = Screen.height;
+            Ray ray = cam.ScreenPointToRay(mouseScreenPos);
 
-            Vector2 minBounds = new Vector2(-screenWidth / 2f, -screenHeight / 2f);
-            Vector2 maxBounds = new Vector2(screenWidth / 2f, screenHeight / 2f);
+            if (Physics.Raycast(ray, out RaycastHit hit, maxDistance))
+            {
+                aimPoint = hit.point;
+                return true;
+            }
 
-            return (minBounds, maxBounds);
+            aimPoint = ray.origin + ray.direction * maxDistance;
+            return false;
         }
     }
 }
