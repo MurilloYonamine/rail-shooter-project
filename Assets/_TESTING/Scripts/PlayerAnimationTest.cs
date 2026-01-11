ae4445b9d7ab362ac95dc5ffb22c607f573c94ee
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2026-01-10 21:43:12 -0300

    feat: add aiming functionality to PlayerAnimationTest and update ArmModelTestScene
    - Introduced new animator parameters for aiming and fire aiming in PlayerAnimationTest.
    - Implemented aiming logic in the Update method, allowing the player to aim using the right mouse button.
    - Updated ArmModelTestScene to include a new component related to player aiming.
    - Adjusted various local Euler angles in the scene for better alignment of game objects.

--- Assets/_TESTING/Scripts/PlayerAnimationTest.cs
+++ Assets/_TESTING/Scripts/PlayerAnimationTest.cs
@@ -8,23 +8,40 @@ namespace RAIL_SHOOTER.PLAYER.TESTING
     {
         private const string ANIMATOR_PARAM_IS_FIRING = "isFiring";
         private const string ANIMATOR_PARAM_IS_RELOADING = "isReloading";
+        private const string ANIMATOR_PARAM_IS_AIMING = "isAiming";
+        private const string ANIMATOR_PARAM_IS_FIRE_AIMING = "isFireAiming";
         [SerializeField] private Animator _armAnimator;
         [SerializeField] private Animator _gunAnimator;
 
         [SerializeField] private KeyCode _fireKey = KeyCode.Mouse0;
+        [SerializeField] private KeyCode _aimKey = KeyCode.Mouse1;
         [SerializeField] private KeyCode _reloadKey = KeyCode.R;
-    
+
 
         private void Update()
         {
+            if (Input.GetKey(_aimKey))
+            {
+                Aim(true);
+            }
+            else
+            {
+                Aim(false);
+            }
+
             if (Input.GetKeyDown(_fireKey))
             {
                 StartCoroutine(Shoot());
             }
+
             if (Input.GetKeyDown(_reloadKey))
             {
                 StartCoroutine(Reload());
             }
+        }
+        private void Aim(bool isAiming = true)
+        {
+            _armAnimator.SetBool(ANIMATOR_PARAM_IS_AIMING, isAiming);
         }
         private IEnumerator Shoot()
         {
@@ -39,7 +56,7 @@ namespace RAIL_SHOOTER.PLAYER.TESTING
         }
         private IEnumerator Reload()
         {
-            float animationDuration = 72f / 60f; 
+            float animationDuration = 72f / 60f;
 
             _armAnimator.SetBool(ANIMATOR_PARAM_IS_RELOADING, true);
             _gunAnimator.SetBool(ANIMATOR_PARAM_IS_RELOADING, true);
